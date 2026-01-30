f5f1b233884defe161f7cb6e503efd58372c0fbe
Ali Shakeel <muhammadalishakeel77@gmail.com>
2026-01-30 05:17:04 +0500

    Add new migrations + seed user

---
+++ db/migrate/20260130090106_add_devise_token_auth_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddDeviseTokenAuthFieldsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :provider, :string, null: false, default: "email"
+    add_column :users, :uid, :string, null: false, default: ""
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
